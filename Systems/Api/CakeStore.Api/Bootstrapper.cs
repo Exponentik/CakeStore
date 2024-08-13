1ccad9b7941732392fbce8d1e72ea5c31c74bd57
padik0801@gmail.com <padik0801@gmail.com>
2024-08-13 16:56:55 +0300

    Workshop1

---
+++ Systems/Api/CakeStore.Api/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace CakeStore.Api;
+using CakeStore.Services.Settings;
+public static class Bootstrapper
+{
+    public static IServiceCollection RegisterServices (this IServiceCollection services)
+    {
+        services.AddMainSettings()
+                .AddSwaggerSettings()
+                .AddLogSettings();
+
+        return services;
+    }
+}
--- Systems/Api/CakeStore.Api/Program.cs
+++ Systems/Api/CakeStore.Api/Program.cs
@@ -1,11 +1,42 @@
+using CakeStore.Api;
+using CakeStore.Api.Configuration;
+using CakeStore.Services.Settings;
+using CakeStore.Services.Logger;
+using CakeStore.Settings;
+var mainSettings = Settings.Load<MainSettings>("Main");
+var logSettings = Settings.Load<LogSettings>("Log");
+var swaggerSettings = Settings.Load<SwaggerSettings>("Swagger");
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddAppLogger(mainSettings, logSettings);
+
+
+var services = builder.Services;
+
+var b = builder.Services;
+
+
+b.AddAppAutoMappers();
+b.AddAppValidator();
+
+b.AddAppHealthChecks();
+
+
+b.RegisterServices();
+b.AddAppVersioning();
+b.AddAppCors ();
+b.AddAppControllerAndViews();
+b.AddAppSwagger(mainSettings, swaggerSettings);
 
 var app = builder.Build();
+app.UseAppHealthChecks();
 
-
-
+app.UseAppCors ();
+app.UseAppControllerAndViews();
+app.UseAppSwagger();
 
 app.Run();
+
 
 
